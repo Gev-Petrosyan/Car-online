95bf09b6f55251793e0eb663dfb30383c5bd9c84
Gevorg-Programmer <git.hub.pg@gmail.com>
2022-04-28 22:31:33 +0400

    New sliders

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,38 @@
 
+new Swiper('.swiper-web', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
 
+new Swiper('.swiper-mobile', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
 
+new Swiper('.swiper-comment-lg', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    slidesPerView: 3
+});
+
+new Swiper('.swiper-comment-md', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    slidesPerView: 2
+});
+
+new Swiper('.swiper-comment-sm', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    slidesPerView: 1
+});
